5f2d1dd5eb4800b91a8de4db38267ec8eaa9c87b
Adelina <if2818@ic.ac.uk>
2021-06-15 23:42:21 +0100

    added auto scroll to about

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import { useRef } from "react";
 import "./App.css";
 import Snowfall from "react-snowfall";
 
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Landing from "./components/Landing";
-
+import Header from "./components/Header";
 
 function App() {
+  const aboutRef = useRef(null);
+  const scrollToAbout = () => {
+    aboutRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
   return (
     <div className="main">
+      <Header scrollToAbout={scrollToAbout} />
       <Snowfall color="#c9576e" snowflakeCount={300} style={{ height: "200vh" }} />
       <Landing />
-      <About />
+      <About ref={aboutRef} />
       <Projects />
     </div>
   );
