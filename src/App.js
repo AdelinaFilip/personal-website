8e3d3b029b496d241fb699d622e7fb894f6fa3ca
Adelina <if2818@ic.ac.uk>
2021-06-15 11:56:46 +0100

    socials working with links

--- src/App.js
+++ src/App.js
@@ -10,14 +10,40 @@ function App() {
         <div className="card">
           <h2 className="title">Hi, I'm Adelina!</h2>
           <div className="socials">
-            <GrMail />
-            <FaLinkedinIn />
-            <FaGithub />
+            <Mailto email="adelinaf21@gmail.com" subject="Hello!">
+              <GrMail />
+            </Mailto>
+            <GoTo link="https://www.linkedin.com/in/ioana-adelina-filip-2b96a0147/">
+              <FaLinkedinIn />
+            </GoTo>
+            <GoTo link="https://github.com/AdelinaFilip">
+              <FaGithub />
+            </GoTo>
           </div>
         </div>
       </Tilt>
     </div>
   );
 }
+
+const Mailto = ({
+  email, subject, body, children
+}) => {
+  return (
+    <a href={
+      `mailto:${email}?subject=${encodeURIComponent(subject) || ""}&body=${encodeURIComponent(body) || ""}`
+    } className="link">
+      {children}
+    </a>
+  );
+};
+
+const GoTo = ({ link, children }) => {
+  return (
+    <a href={`${link}`} className="link" target="_blank" rel="noreferrer">
+      {children}
+    </a>
+  );
+};
 
 export default App;
