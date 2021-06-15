44c407fcc8f5cf3c395f9917ed215635291d5585
Adelina <if2818@ic.ac.uk>
2021-06-16 00:39:51 +0100

    changed projects layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css";
 function Header({ scrollToAbout, scrollToProjects }) {
   return (
     <div className="header">
-      <p className="header-element" onClick={scrollToAbout}>About me</p>
+      <p className="header-element" onClick={scrollToAbout}>About</p>
       <p className="header-element" onClick={scrollToProjects}>Projects</p>
     </div>
   );
