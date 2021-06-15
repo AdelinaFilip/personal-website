4a292009b086f2ccf33b8a66fec69f418c3515c8
Adelina <if2818@ic.ac.uk>
2021-06-15 11:20:43 +0100

    added socials and tilt

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { GrMail } from "react-icons/gr";
+import Tilt from "react-parallax-tilt";
 
 function App() {
   return (
     <div className="main">
-      <h2 className="title">Hi, I'm Adelina!</h2>
+      <Tilt tiltMaxAngleX={15} tiltMaxAngleY={15}>
+        <div className="card">
+          <h2 className="title">Hi, I'm Adelina!</h2>
+          <div className="socials">
+            <GrMail />
+            <FaLinkedinIn />
+            <FaGithub />
+          </div>
+        </div>
+      </Tilt>
     </div>
   );
 }
