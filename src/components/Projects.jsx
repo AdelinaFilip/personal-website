09bb45992b4924005389257c51be4cd44c0fad31
Adelina <if2818@ic.ac.uk>
2021-06-16 10:44:49 +0100

    added colour to active element in project panel

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,12 @@
 import { forwardRef } from "react";
 import "./Projects.css";
+import ProjectsPanel from "./ProjectsPanel";
 
 
 
 const Projects = forwardRef((_, ref) => (
   <div ref={ref} className="projects-main">
-    <div className="projects-box">
-      <div className="controls">
-        <p className="controls-text">University</p>
-        <p className="controls-text">Others</p>
-      </div>
-      {/* <vl className="projects-divider" /> */}
-      <p className="projects-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id aliquam metus. Maecenas eget auctor tortor, at ullamcorper magna. Proin tristique mi auctor maximus elementum. Donec porttitor sem non purus egestas, in maximus nisi condimentum. Donec iaculis sagittis magna, eu cursus turpis tempor eu. Cras ultrices dapibus scelerisque. Integer et fringilla lacus, in vestibulum ipsum. Sed id risus non nunc finibus condimentum. Sed bibendum, neque quis facilisis laoreet, lacus ligula vulputate sem, non aliquam eros nulla sed neque. Aliquam erat volutpat. Vestibulum convallis sit amet nibh consectetur facilisis. Praesent risus tortor, efficitur id efficitur in, faucibus sed ante. Nunc tincidunt nunc id ligula cursus, at faucibus velit rutrum. Vestibulum metus mi, consequat in tempor nec, volutpat vel dolor. Aenean rhoncus tempor felis eu efficitur.
-      </p>
-    </div>
+    <ProjectsPanel />
   </div>
 ));
 
