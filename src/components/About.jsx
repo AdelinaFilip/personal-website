cdd4ddd75f48143f44c76b67867b49f3bb3626f0
Adelina <if2818@ic.ac.uk>
2021-09-02 17:00:37 +0100

    added intro

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,9 +8,11 @@ import {
 import FadeInSection from "../utils/FadeIn";
 
 import "./About.css";
-const intro = "As you have probably seen above, my name is Adelina. I am currently a final year student at Imperial College London. Over the past couple of years I have worked ";
 
 const About = forwardRef((_, ref) => {
+
+  const intro = "As you have probably seen above, my name is Adelina. I am currently a final year student at Imperial College London. My main interest is software engineering, and I have been fidding with code ever since I was in high-school, when I first came accross C++ programming and web development. Over the past couple of years, I have worked on both frontend and backend projects, which allowed me to develop a breadth of skills. \n Away from the computer, I am an avid food enthusiasts (who unfortunatelly can't cook), and I also enjoy taking photographs everywhere I go.";
+
   return (
     <div ref={ref} className="about-main" >
       <FadeInSection>
